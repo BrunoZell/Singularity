de81ad779b896ad1c1cd36f2144a3434a1303737
Bruno Zell <bruno.zzell@gmail.com>
2020-03-21 17:05:45 +0100

    duplicate init using a child container

--- src/Singularity.DoubleInit/Program.cs
+++ src/Singularity.DoubleInit/Program.cs
@@ -15,16 +15,16 @@ namespace Singularity.DoubleInit
         {
             var container = new Container(builder =>
             {
-                // This registration does result in two initializations of WritingSingleton
-                //builder.Register<ISingleton, WritingSingleton>(c => c.As<WritingSingleton>().With(Lifetimes.PerContainer));
-
-                // This registration does result in one initialization of WritingSingleton
-                builder.Register(typeof(WritingSingleton), c => c.As(typeof(ISingleton)).With(Lifetimes.PerContainer));
+                // Both registrations result in two initializations of WritingSingleton
+                builder.Register<ISingleton, WritingSingleton>(c => c.As<WritingSingleton>().With(Lifetimes.PerContainer));
+                //builder.Register(typeof(WritingSingleton), c => c.As(typeof(ISingleton)).With(Lifetimes.PerContainer));
             });
 
-            var instance1 = container.GetInstance<ISingleton>();
-            var instance2 = container.GetInstance<ISingleton>();
-            var instance3 = container.GetInstance<WritingSingleton>();
+            var childContainer = container.GetNestedContainer();
+
+            var instance1 = childContainer.GetInstance<ISingleton>();
+            var instance2 = childContainer.GetInstance<ISingleton>();
+            var instance3 = childContainer.GetInstance<WritingSingleton>();
         }
     }
 }
